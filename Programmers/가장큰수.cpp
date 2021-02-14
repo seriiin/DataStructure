73c317e4137ca1f7d76ec76dec0b063f13a9a6de
seriiin <selin@choeselin-ui-MacBookPro.local>
2021-02-14 19:08:49 +0900

    가장큰수

--- Programmers/가장큰수.cpp
+++ Programmers/가장큰수.cpp
@@ -2,19 +2,23 @@
 #include <vector>
 #include <algorithm>
 using namespace std;
-bool re(int x, int y){
-    return x>y;
+
+bool cmp(string a, string b) {
+    return a+b > b+a;
 }
 string solution(vector<int> numbers) {
     string answer = "";
-    vector<string> numbers;
+    vector<string> tmp;
 
-    sort(numbers.begin(),numbers.end(),re);
+    for(int i=0;i<numbers.size();i++)
+        tmp.push_back(to_string(numbers[i]));
     
+    sort(tmp.begin(), tmp.end(), cmp);
     
-    
-    for(int i=0;i<numbers.size();i++){
-        answer+=to_string(numbers[i]);
-    }
+    if(tmp[0]=="0")
+        return "0";
+    else
+        for(int i=0;i<numbers.size();i++)
+            answer+=tmp[i];
     return answer;
-}
+}}
